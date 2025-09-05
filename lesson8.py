4612ac895a3c15b11e0fb66d9dadcc7d660f3cc0
Polina Vasilyeva <Polinova31@gmail.com>
2025-09-05 16:17:15 +0300

    Add files via upload

--- lesson8.py
+++ lesson8.py
@@ -7,6 +7,7 @@ from pprint import pprint
 import folium
 load_dotenv()
 
+
 def fetch_coordinates(apikey, address):
     base_url = "https://geocode-maps.yandex.ru/1.x"
     response = requests.get(base_url, params={
@@ -25,46 +26,55 @@ def fetch_coordinates(apikey, address):
     return lon, lat
 
 
-with open("coffee.json", "r", encoding="CP1251") as my_file:
-	file_content = my_file.read()
-file_cont = json.loads(file_content)
+def load_coffee_shops():
+	with open("coffee.json", "r", encoding="CP1251") as my_file:
+		file_content = my_file.read()
+	return json.loads(file_content)
 
-coffee_shops_list = []
-for files in file_cont:
-	coffee_shop = {
-	'latitude' : files["geoData"]['coordinates'][1],
-	'longitude' : files["geoData"]['coordinates'][0],
-	'title': files["Name"]
-	}
-	coffee_shops_list.append(coffee_shop)
+def get_coffee_list(coffee_data):
+	coffee_shops_list = []
+	for shop in coffee_data:
+		coffee_shop = {
+		'latitude' : files["geoData"]['coordinates'][1],
+		'longitude' : files["geoData"]['coordinates'][0],
+		'title': files["Name"]
+		}
+		coffee_shops_list.append(coffee_shop)
+	return coffee_shops_list	
 
+def main():
+	coffee_data = load_coffee_shops()
+	coffee_shops_list = get_coffee_list(coffee_data)
 
-town = input("Где вы находителсь?")
-apikey = os.getenv('apikey')
-location = fetch_coordinates(apikey, town)
-
-for files in coffee_shops_list:
-	shop_coord = (files['longitude'], files['latitude']) 
-	files['distance'] = distance(lonlat(*location), lonlat(*shop_coord)).km
-
-sort_coffee_shops = sorted(coffee_shops_list, key=lambda x: x['distance'])
-near_coffee_shops = sort_coffee_shops[:5]
-pprint(near_coffee_shops)
-
-m = folium.Map(location=[location[1], location[0]])
-
-for shop in near_coffee_shops:
-	folium.Marker(
-		location = [shop['latitude'],shop['longitude']],
-		tooltip = "Click me",
-		popup = shop['title'],
-		icon = folium.Icon(color = "pink")
-	).add_to(m)
-
-m.save('coffee.html')
+	town = input("Где вы находителсь?")
+	apikey = os.getenv('apikey')
+	location = fetch_coordinates(apikey, town)
 
 
+	for shop in coffee_shops_list:
+		shop_coord = (files['longitude'], files['latitude']) 
+		files['distance'] = distance(lonlat(*location), lonlat(*shop_coord)).km
 
+
+	sort_coffee_shops = sorted(coffee_shops_list, key=lambda x: x['distance'])
+	near_coffee_shops = sort_coffee_shops[:5]
+
+
+	m = folium.Map(location=[location[1], location[0]])
+
+	for shop in near_coffee_shops:
+		folium.Marker(
+			location = [shop['latitude'],shop['longitude']],
+			tooltip = "Click me",
+			popup = shop['title'],
+			icon = folium.Icon(color = "pink")
+		).add_to(m)
+
+	m.save('coffee.html')
+
+
+if __name__ == __main__:
+	main()
 
 
 
